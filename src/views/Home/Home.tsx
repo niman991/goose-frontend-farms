4e74106758bea3809a5e932ae67542682809aff1
niman991 <82784468+niman991@users.noreply.github.com>
2021-07-16 00:58:19 +0200

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -57,7 +57,6 @@ const Home: React.FC = () => {
       <div>
         <Cards>
           <FarmStakingCard />
-          <TwitterCard/>
           <CakeStats />
           <TotalValueLockedCard />
         </Cards>
