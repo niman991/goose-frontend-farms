6ed176268d7f617e3cb5f8c6e0c5c525aa8a31a1
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-12 20:06:30 +0200

    Update Global.tsx

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Kanit', sans-serif;
   }
   body {
+    background-image: url("bg.png");
     background-color: ${({ theme }) => theme.colors.background};
     background-repeat: no-repeat;
     background-position: center;
