eccfe4646c40f0155e1f211ca8e9e269f1b748b5
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-17 00:57:45 +0200

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -2,14 +2,17 @@ export default {
   cake: {
     56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
     97: '',
+    1285: '0xd988CF2BF3c29a69470457392b9Bd65ABCB7d3B5',
   },
   masterChef: {
     56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
     97: '',
+    1285: '0x199bE1B0221C7CFb14dD05a4eC62be0acDdEd8B1',
   },
   wbnb: {
     56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     97: '',
+    1285: '0x98878B06940aE243284CA214f92Bb71a2b032B8A'
   },
   lottery: {
     56: '',
@@ -22,9 +25,11 @@ export default {
   mulltiCall: {
     56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
     97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+    1285: '0xB5C742EE058209c0bC828c71cb9b1f63799C3E5C'
   },
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
     97: '',
+    1285: '0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D'
   },
 }
