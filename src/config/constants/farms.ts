23418705cdcf3622e3bc7fde1cc099d0fd9edecc
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-17 21:30:47 +0200

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -9,13 +9,13 @@ const farms: FarmConfig[] = [
     lpAddresses: {
       97: '',
       56: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      137: '0x132fb92F49F3e1bD05624146801a78B6ddaf25fE'
+      1285: '0x08d7A1C07766678A3C5560eeb13cdA746e315D1A'
     },
     tokenSymbol: 'RIVA',
     tokenAddresses: {
       97: '',
       56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
-      137: '0xb042Ca7c7c5785EDb543D3323097e2E30e505aCE',
+      1285: '0xd988cf2bf3c29a69470457392b9bd65abcb7d3b5',
     },
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
@@ -23,17 +23,17 @@ const farms: FarmConfig[] = [
   {
     pid: 1,
     risk: 5,
-    lpSymbol: 'RIVA-MATIC LP',
+    lpSymbol: 'RIVA-WMOVR LP',
     lpAddresses: {
       97: '',
       56: '0xd1b59d11316e87c3a0a069e80f590ba35cd8d8d3',
-      137: '0xA9A22a2C619C95D16FCb9f19878560DA973d755f'
+      1285: '0x0De00aF33c7a169bCDff5403BF13075a2AEd60DA'
     },
     tokenSymbol: 'RIVA',
     tokenAddresses: {
       97: '',
       56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
-      137: '0xb042Ca7c7c5785EDb543D3323097e2E30e505aCE',
+      1285: '0xd988cf2bf3c29a69470457392b9bd65abcb7d3b5',
     },
     quoteTokenSymbol: QuoteToken.BNB,
     quoteTokenAdresses: contracts.wbnb,
@@ -45,13 +45,13 @@ const farms: FarmConfig[] = [
     lpAddresses: {
       97: '',
       56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
-      137: '0x6e7a5fafcec6bb1e78bae2a1f0b612012bf14827',
+      1285: '0x74888A02891586EBecCc7B04A0F7a9b5098Daf05',
     },
     tokenSymbol: 'MATIC',
     tokenAddresses: {
       97: '',
       56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
-      137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+      1285: '0x98878b06940ae243284ca214f92bb71a2b032b8a',
     },
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
