11729a52fc6e388094364a4c72308d9a8ab8b400
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-17 17:51:54 +0200

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -5,13 +5,13 @@ const farms: FarmConfig[] = [
   {
     pid: 0,
     risk: 5,
-    lpSymbol: 'EGG-BUSD LP',
+    lpSymbol: 'RIVA-USDC LP',
     lpAddresses: {
       97: '',
       56: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
       137: '0x132fb92F49F3e1bD05624146801a78B6ddaf25fE'
     },
-    tokenSymbol: 'EGG',
+    tokenSymbol: 'RIVA',
     tokenAddresses: {
       97: '',
       56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
@@ -23,13 +23,13 @@ const farms: FarmConfig[] = [
   {
     pid: 1,
     risk: 5,
-    lpSymbol: 'EGG-BNB LP',
+    lpSymbol: 'RIVA-MATIC LP',
     lpAddresses: {
       97: '',
       56: '0xd1b59d11316e87c3a0a069e80f590ba35cd8d8d3',
       137: '0xA9A22a2C619C95D16FCb9f19878560DA973d755f'
     },
-    tokenSymbol: 'EGG',
+    tokenSymbol: 'RIVA',
     tokenAddresses: {
       97: '',
       56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
@@ -41,13 +41,13 @@ const farms: FarmConfig[] = [
   {
     pid: 2,
     risk: 3,
-    lpSymbol: 'BNB-BUSD LP',
+    lpSymbol: 'MATIC-USDC LP',
     lpAddresses: {
       97: '',
       56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
       137: '0x6e7a5fafcec6bb1e78bae2a1f0b612012bf14827',
     },
-    tokenSymbol: 'BNB',
+    tokenSymbol: 'MATIC',
     tokenAddresses: {
       97: '',
       56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
