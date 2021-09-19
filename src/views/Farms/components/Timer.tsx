47d55c142d3c46faee3ab7070afb218321465482
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-20 01:36:43 +0200

    Update Timer.tsx

--- src/views/Farms/components/Timer.tsx
+++ src/views/Farms/components/Timer.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Countdown from 'react-countdown';
+import React from "react";
+import ReactDOM from "react-dom";
+import Countdown from "react-countdown";
+import styled from 'styled-components'
+
+
+
+const TimerBox = styled.div`
+  display: flex;
+  justify-content: space-around;
+  font-family: sans-serif;
+  text-align: center;
+  padding-top: 20px;
+  margin-bottom: 40px;
+  `
+
 
 const Timer = () => {
-  ReactDOM.render(
-  <Countdown date={Date.now() + 259200000} />,
+ReactDOM.render(
+  <Countdown date={Date.now() + 1000000} />,
   document.getElementById("root")
 );
-}
 
-export default Timer
+  return (
+  <TimerBox>
+    <Timer>
+    </Timer>
+  </TimerBox>
+  )
+  
+};
+
+
+
+
+export default Timer;
