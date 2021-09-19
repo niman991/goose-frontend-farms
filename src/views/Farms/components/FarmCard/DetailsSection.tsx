d2ac7ac1572433bc292ace90fca093843ee69791
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-19 04:14:38 +0200

    Update DetailsSection.tsx

--- src/views/Farms/components/FarmCard/DetailsSection.tsx
+++ src/views/Farms/components/FarmCard/DetailsSection.tsx
@@ -54,9 +54,9 @@ const DetailsSection: React.FC<ExpandableSectionProps> = ({
         <Text>{TranslateString(316, 'Stake')}:</Text>
         <StyledLinkExternal href={
           isTokenOnly ?
-            `https://exchange.goosedefi.com/#/swap/${tokenAddresses[process.env.REACT_APP_CHAIN_ID]}`
+            `https://swap.moonswap.in/#/swap${tokenAddresses[process.env.REACT_APP_CHAIN_ID]}`
             :
-          `https://exchange.goosedefi.com/#/add/${liquidityUrlPathParts}`
+          `https://swap.moonswap.in/#/add/${liquidityUrlPathParts}`
         }>
           {lpLabel}
         </StyledLinkExternal>
