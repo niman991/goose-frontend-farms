2ac2dcd62078d777bdafe2764464d4fefa57c0b1
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-20 00:32:15 +0200

    Create Timer.tsx

---
+++ src/views/Farms/components/Timer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Countdown from "react-countdown";
+
+ReactDOM.render(
+  <Countdown date={Date.now() + 259200000} />,
+  document.getElementById("root")
+);
