6acf89716596c5245806808fb6ad0f1cfde13300
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-20 03:33:59 +0200

    Update Farms.tsx

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -16,7 +16,7 @@ import useI18n from 'hooks/useI18n'
 import FarmCard, { FarmWithStakedValue } from './components/FarmCard/FarmCard'
 import FarmTabButtons from './components/FarmTabButtons'
 import Divider from './components/Divider'
-import Timer from '.components/Timer'
+import Timer from './components/Timer'
 
 
 
