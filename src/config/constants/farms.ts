afd52190d8e48b5e73154b60620a34997dbe8446
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-24 02:02:09 +0200

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -41,13 +41,13 @@ const farms: FarmConfig[] = [
   {
     pid: 2,
     risk: 3,
-    lpSymbol: 'MATIC-USDC LP',
+    lpSymbol: 'MOVR-USDC LP',
     lpAddresses: {
       97: '',
       56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
       137: '0x6e7a5fafcec6bb1e78bae2a1f0b612012bf14827',
     },
-    tokenSymbol: 'MATIC',
+    tokenSymbol: 'MOVR',
     tokenAddresses: {
       97: '',
       56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
@@ -56,6 +56,148 @@ const farms: FarmConfig[] = [
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
   },
+    {
+    pid: 3,
+    risk: 4,
+    lpSymbol: 'MSWAP-MOVR LP',
+    lpAddresses: {
+      97: '',
+      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+      137: '0x8BaFdF3a3fAEF45A08D2bc065e736D3D8c6FDFE7',
+    },
+    tokenSymbol: 'MSWAP',
+    tokenAddresses: {
+      97: '',
+      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+      137: '0xF4B0903774532AEe5ee567C02aaB681a81539e92',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+    {
+    pid: 4,
+    risk: 3,
+    lpSymbol: 'WETH-USDC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+      137: '0x853ee4b2a13f8a742d64c8f088be7ba2131f670d',
+    },
+    tokenSymbol: 'WETH',
+    tokenAddresses: {
+      97: '',
+      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+      137: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  }, 
+      {
+    pid: 5,
+    risk: 3,
+    lpSymbol: 'WBTC-USDC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+      137: '0x99dADf889520b9cEBD9959d3ca1866DC2517Ea1d',
+    },
+    tokenSymbol: 'WBTC',
+    tokenAddresses: {
+      97: '',
+      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+      137: '0xF4B0903774532AEe5ee567C02aaB681a81539e92',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  },
+  
+  
+  
+  
+  
+  
+  
+      {
+    pid: 6,
+    risk: 1,
+    lpSymbol: 'BUSD-USDC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+      137: '0xB3FB48bF090bEDFF4f6F93FFb40221742E107db7',
+    },
+    tokenSymbol: 'BUSD',
+    tokenAddresses: {
+      97: '',
+      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+      137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  },
+      {
+    pid: 7,
+    risk: 1,
+    lpSymbol: 'USDT-USDC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+      137: '0xB3FB48bF090bEDFF4f6F93FFb40221742E107db7',
+    },
+    tokenSymbol: 'USDT',
+    tokenAddresses: {
+      97: '',
+      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+      137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  },
+      {
+    pid: 8,
+    risk: 1,
+    lpSymbol: 'DAI-USDC LP',
+    lpAddresses: {
+      97: '',
+      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+      137: '0xB3FB48bF090bEDFF4f6F93FFb40221742E107db7',
+    },
+    tokenSymbol: 'DAI',
+    tokenAddresses: {
+      97: '',
+      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+      137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  },
+  
+  
+  
+  
+    {
+    pid: 2,
+    risk: 5,
+    isTokenOnly: true,
+    lpSymbol: 'RIVA',
+    lpAddresses: {
+      97: '',
+      56: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      137: '0x96C5b8fa9c0432c22AFBFA8FF03d6f3DeEFAd78b', // KRILL - USDC
+    },
+    tokenSymbol: 'RIVA',
+    tokenAddresses: {
+      97: '',
+      56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+      137: '0xdd31D406AD1a88c000E1Ad7ee7d277157C9D7d13',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  },
+  
+  
+  
+  
 ]
 
 export default farms
