3f42840102b9a176291f8c183a208e55ba9b6c97
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-25 02:47:03 +0200

    Update FarmCard.tsx

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -117,7 +117,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
     : '-'
 
   const lpLabel = farm.lpSymbol
-  const earnLabel = 'RIV'
+  const earnLabel = 'RIVA'
   const farmAPY = farm.apy && farm.apy.times(new BigNumber(100)).toNumber().toLocaleString(undefined, {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
