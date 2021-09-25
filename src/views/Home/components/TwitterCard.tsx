bfb6d0147357b13e6279ca2900685fb5af5a5f29
niman991 <82784468+niman991@users.noreply.github.com>
2021-09-25 02:59:20 +0200

    Update TwitterCard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'RiverRocket_xyz'
           }}
           options={{
             height: '300',
