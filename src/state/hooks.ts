7e1bdafb104a3048523906854fe2df2814cd0f59
niman991 <82784468+niman991@users.noreply.github.com>
2021-10-04 00:12:19 +0200

    Update hooks.ts

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -101,7 +101,7 @@ export const useTotalValue = (): BigNumber => {
       let val;
       if (farm.quoteTokenSymbol === QuoteToken.BNB) {
         val = (bnbPrice.times(farm.lpTotalInQuoteToken));
-      }else if (farm.quoteTokenSymbol === QuoteToken.CAKE) {
+      }else if (farm.quoteTokenSymbol === QuoteToken.BUSD) {
         val = (cakePrice.times(farm.lpTotalInQuoteToken));
       }else{
         val = (farm.lpTotalInQuoteToken);
