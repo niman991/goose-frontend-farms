45af11455d3bec07c6446861c78ab2122521f5dc
niman991 <82784468+niman991@users.noreply.github.com>
2021-10-08 03:19:22 +0200

    Update fetchFarms.ts

--- src/state/farms/fetchFarms.ts
+++ src/state/farms/fetchFarms.ts
@@ -2,29 +2,32 @@ import BigNumber from 'bignumber.js'
 import erc20 from 'config/abi/erc20.json'
 import masterchefABI from 'config/abi/masterchef.json'
 import multicall from 'utils/multicall'
-import { getAddress, getMasterChefAddress } from 'utils/addressHelpers'
+import { getMasterChefAddress } from 'utils/addressHelpers'
 import farmsConfig from 'config/constants/farms'
+import { QuoteToken } from '../../config/constants/types'
+
+const CHAIN_ID = process.env.REACT_APP_CHAIN_ID
 
 const fetchFarms = async () => {
   const data = await Promise.all(
     farmsConfig.map(async (farmConfig) => {
-      const lpAdress = getAddress(farmConfig.lpAddresses)
+      const lpAdress = farmConfig.lpAddresses[CHAIN_ID]
       const calls = [
         // Balance of token in the LP contract
         {
-          address: getAddress(farmConfig.tokenAddresses),
+          address: farmConfig.tokenAddresses[CHAIN_ID],
           name: 'balanceOf',
           params: [lpAdress],
         },
         // Balance of quote token on LP contract
         {
-          address: getAddress(farmConfig.quoteTokenAdresses),
+          address: farmConfig.quoteTokenAdresses[CHAIN_ID],
           name: 'balanceOf',
           params: [lpAdress],
         },
         // Balance of LP tokens in the master chef contract
         {
-          address: lpAdress,
+          address: farmConfig.isTokenOnly ? farmConfig.tokenAddresses[CHAIN_ID] : lpAdress,
           name: 'balanceOf',
           params: [getMasterChefAddress()],
         },
@@ -35,12 +38,12 @@ const fetchFarms = async () => {
         },
         // Token decimals
         {
-          address: getAddress(farmConfig.tokenAddresses),
+          address: farmConfig.tokenAddresses[CHAIN_ID],
           name: 'decimals',
         },
         // Quote token decimals
         {
-          address: getAddress(farmConfig.quoteTokenAdresses),
+          address: farmConfig.quoteTokenAdresses[CHAIN_ID],
           name: 'decimals',
         },
       ]
@@ -51,25 +54,49 @@ const fetchFarms = async () => {
         lpTokenBalanceMC,
         lpTotalSupply,
         tokenDecimals,
-        quoteTokenDecimals,
+        quoteTokenDecimals
       ] = await multicall(erc20, calls)
 
-      // Ratio in % a LP tokens that are in staking, vs the total number in circulation
-      const lpTokenRatio = new BigNumber(lpTokenBalanceMC).div(new BigNumber(lpTotalSupply))
+    
 
-      // Total value in staking in quote token value
-      const lpTotalInQuoteToken = new BigNumber(quoteTokenBlanceLP)
-        .div(new BigNumber(10).pow(18))
-        .times(new BigNumber(2))
-        .times(lpTokenRatio)
+      let tokenAmount;
+      let lpTotalInQuoteToken;
+      let tokenPriceVsQuote;
+      if (farmConfig.isTokenOnly) {
+      //     console.log('here2',
+      //   tokenDecimals,farmConfig
+      // )
+        tokenAmount = new BigNumber(lpTokenBalanceMC).div(new BigNumber(10).pow(6));
+        if(farmConfig.tokenSymbol === QuoteToken.BUSD && farmConfig.quoteTokenSymbol === QuoteToken.BUSD){
+          tokenPriceVsQuote = new BigNumber(1);
+        }else{
+          tokenPriceVsQuote = new BigNumber(quoteTokenBlanceLP).div(new BigNumber(tokenBalanceLP));
+        }
+        lpTotalInQuoteToken = tokenAmount.times(tokenPriceVsQuote);
+      }else{
+        // Ratio in % a LP tokens that are in staking, vs the total number in circulation
+        const lpTokenRatio = new BigNumber(lpTokenBalanceMC).div(new BigNumber(lpTotalSupply))
 
-      // Amount of token in the LP that are considered staking (i.e amount of token * lp ratio)
-      const tokenAmount = new BigNumber(tokenBalanceLP).div(new BigNumber(10).pow(tokenDecimals)).times(lpTokenRatio)
-      const quoteTokenAmount = new BigNumber(quoteTokenBlanceLP)
-        .div(new BigNumber(10).pow(quoteTokenDecimals))
-        .times(lpTokenRatio)
+        // Total value in staking in quote token value
+        lpTotalInQuoteToken = new BigNumber(quoteTokenBlanceLP)
+         // .div(new BigNumber(10).pow(6))
+          .times(new BigNumber(2))
+          .times(lpTokenRatio)
 
-      const [info, totalAllocPoint] = await multicall(masterchefABI, [
+        // Amount of token in the LP that are considered staking (i.e amount of token * lp ratio)
+        tokenAmount = new BigNumber(tokenBalanceLP).div(new BigNumber(10).pow(tokenDecimals)).times(lpTokenRatio)
+        const quoteTokenAmount = new BigNumber(quoteTokenBlanceLP)
+          .div(new BigNumber(10).pow(quoteTokenDecimals))
+          .times(lpTokenRatio)
+
+        if(tokenAmount.comparedTo(0) > 0){
+          tokenPriceVsQuote = quoteTokenAmount.div(tokenAmount);
+        }else{
+          tokenPriceVsQuote = new BigNumber(quoteTokenBlanceLP).div(new BigNumber(tokenBalanceLP));
+        }
+      }
+
+      const [info, totalAllocPoint, rivaPerBlock] = await multicall(masterchefABI, [
         {
           address: getMasterChefAddress(),
           name: 'poolInfo',
@@ -79,6 +106,10 @@ const fetchFarms = async () => {
           address: getMasterChefAddress(),
           name: 'totalAllocPoint',
         },
+        {
+          address: getMasterChefAddress(),
+          name: 'rivaPerBlock',
+        },
       ])
 
       const allocPoint = new BigNumber(info.allocPoint._hex)
@@ -87,11 +118,13 @@ const fetchFarms = async () => {
       return {
         ...farmConfig,
         tokenAmount: tokenAmount.toJSON(),
-        quoteTokenAmount: quoteTokenAmount.toJSON(),
+        // quoteTokenAmount: quoteTokenAmount,
         lpTotalInQuoteToken: lpTotalInQuoteToken.toJSON(),
-        tokenPriceVsQuote: quoteTokenAmount.div(tokenAmount).toJSON(),
-        poolWeight: poolWeight.toJSON(),
+        tokenPriceVsQuote: tokenPriceVsQuote.toJSON(),
+        poolWeight: poolWeight.toNumber(),
         multiplier: `${allocPoint.div(100).toString()}X`,
+        depositFeeBP: info.depositFeeBP,
+        rivaPerBlock: new BigNumber(rivaPerBlock).toNumber(),
       }
     }),
   )
